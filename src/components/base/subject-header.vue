c58ae5873052a4a805d9d7cd9a685fae6eab613e
张应锋 <zhangyingfeng@hogecloud.com>
2018-11-15 14:54:43 +0800

    添加基础试题列表组件和图片资源

--- src/components/base/subject-header.vue
+++ src/components/base/subject-header.vue
@@ -1,22 +1,59 @@
 <template lang="html">
   <div class="subject-header-wrap">
     <!--试题概览数据-->
-    <div class="subject-item-wrap">
-      <div class="item" :class="[getItemClass(index)]" v-for="(item,index) in 20" :key="index">{{item}}</div>
+    <div class="subject-item-wrap" ref="subjectItemWrap">
+      <div class="item success" ref="subjectItem" :class="{active: currentActiveIndex == index}"  v-for="(item,index) in subjectList" :key="index">{{item.val}}</div>
     </div>
     <!--缩略按钮-->
-    <div class="thumb-btn">
+    <div class="thumb-btn" @click.stop="toggleSubjectList">
       <div class="line-wrap">
         <span class="line" v-for="(item,index) in 3" :key="index"></span>
       </div>
     </div>
+    <!--题目缩略列表展示-->
+    <transition name="down" mode="out-in">
+      <div class="subject-list-wrap" v-show="isShowSubjectList">
+        <!--头部包裹-->
+        <div class="title-wrap">
+          <span class="title">所有试题</span>
+          <span class="close" @click.stop="toggleSubjectList"></span>
+        </div>
+        <!--底部列表-->
+        <subject-list class="list-wrap" :list="subjectList" @close="toggleSubjectList" @select="selectSubject"></subject-list>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
+import SubjectList from './subject-list'
+
 export default {
   name: 'subject-header',
+  data () {
+    return {
+      isShowSubjectList: false,
+      currentActiveIndex: 0,
+      subjectList: []
+    }
+  },
+  components: {
+    SubjectList
+  },
+  created () {
+    this._mockData()
+  },
   methods: {
+    _mockData () {
+      let list = []
+      for (let i = 0; i < 24; i++) {
+        list.push({
+          key: `key_${i}`,
+          val: i + 1
+        })
+      }
+      this.subjectList = list
+    },
     getItemClass (index) {
       let i = index % 4
       let cls = 'success'
@@ -24,6 +61,15 @@ export default {
       else if (i === 2) cls = 'disabled'
       if (i === 3) cls = 'active'
       return cls
+    },
+    toggleSubjectList () {
+      this.isShowSubjectList = !this.isShowSubjectList
+    },
+    selectSubject (index) {
+      let subjectScrollEl = this.$refs.subjectItemWrap
+      let subjectItem = this.$refs.subjectItem[index]
+      this.currentActiveIndex = index
+      subjectScrollEl.scrollTo(subjectItem.offsetLeft - 15, 0)
     }
   }
 }
@@ -54,6 +100,9 @@ export default {
       border-radius: 50%;
       margin-left: px2rem(30px);
       @include font-dpr(14px);
+      &:last-child{
+        margin-right: px2rem(30px);
+      }
       &.success{
         @include bg-color('bgColor');
         @include font-color('themeColor');
@@ -99,5 +148,39 @@ export default {
       }
     }
   }
+  .subject-list-wrap{
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 99;
+    @include bg-color('bgColor');
+    .title-wrap{
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: px2rem(38px) px2rem(41px) px2rem(38px) px2rem(31px);
+      margin-bottom: px2rem(28px);
+      box-sizing: border-box;
+      .title{
+        @include font-dpr(15px);
+        @include font-color('titleColor');
+      }
+      .close{
+        display: block;
+        width: px2rem(28px);
+        height: px2rem(28px);
+        @include img-retina('~@/assets/common/close@2x.png','~@/assets/common/close@3x.png', 100%, 100%);
+        background-repeat: no-repeat;
+        background-position: center;
+      }
+    }
+    .list-wrap{
+      padding:0 px2rem(36px);
+      box-sizing: border-box;
+    }
+  }
 }
+
 </style>
---
+++ src/components/base/subject-list.vue
@@ -0,0 +1,118 @@
+<template lang="html">
+  <!--底部列表-->
+  <div class="list-wrap" v-if="list.length">
+    <div class="row-wrap" v-for="(row,index) in rows" :key='index' ref="subjectRow">
+      <div class="item success" v-for="item in row" :key="item.key" @click.stop="jumpSubject(item.key)">{{item.val}}</div>
+      <!--空占位-->
+      <template v-if="row.length < limitCols && subjectEmptyItems">
+        <div class="place-item"  v-for="(emptyItem,emptyIndex) in subjectEmptyItems" :key="emptyIndex"></div>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'subject-list',
+  props: {
+    list: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data () {
+    return {
+      limitCols: 5,
+      subjectEmptyItems: null // 空占位
+    }
+  },
+  computed: {
+    rows () {
+      // 建立一个mock的数据
+      let limitCols = this.limitCols
+      let list = this.list
+      // 处理下数据
+      let resultArr = []
+      let rowArr = []
+      list.forEach((item, index) => {
+        if (index % limitCols === 0 && index !== 0) {
+          resultArr.push(rowArr)
+          rowArr = [ item ]
+        } else {
+          rowArr.push(item)
+        }
+      })
+
+      if (rowArr.length) {
+        resultArr.push(rowArr)
+        rowArr = []
+      }
+      return resultArr
+    }
+  },
+  mounted () {
+    this.getEmptyItem()
+  },
+  methods: {
+    getEmptyItem () {
+      this.$nextTick(() => {
+        let prevRowIndex = this.rows.length - 2
+        let curRowIndex = this.rows.length - 1
+        let prevRowEl = this.$refs.subjectRow[prevRowIndex]
+        let curRowEl = this.$refs.subjectRow[curRowIndex]
+        let detalItemNum = prevRowEl.children.length - curRowEl.children.length
+        // console.log('列表空白填充个数', detalItemNum)
+        this.subjectEmptyItems = detalItemNum
+      })
+    },
+    close () {
+      this.$emit('close')
+    },
+    jumpSubject (key) {
+      let selectIndex = this.list.findIndex(item => item.key === key)
+      this.$emit('select', selectIndex)
+      this.close()
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "@/styles/index.scss";
+
+.list-wrap{
+  .row-wrap{
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: px2rem(36px);
+    .item,.place-item{
+      text-align: center;
+      line-height: px2rem(100px);
+      width: px2rem(100px);
+      height: px2rem(100px);
+      border-radius: 50%;
+      @include font-dpr(16px);
+      &.success{
+        @include bg-color('bgColor');
+        @include font-color('themeColor');
+        @include border('all',1px,solid,'themeColor');
+      }
+      &.error{
+        @include bg-color('bgColor');
+        @include font-color('errorColor');
+        @include border('all',1px,solid,'errorColor');
+      }
+      &.disabled{
+        @include bg-color('bgGrayColor');
+        @include font-color('disabledColor');
+        @include border('all',1px,solid,'borderGray');
+      }
+      &.active{
+        @include bg-color('activeColor');
+        @include font-color('bgColor');
+        @include border('all',1px,solid,'activeColor');
+      }
+    }
+  }
+}
+</style>
