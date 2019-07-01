5521f9f564774179b00e614e8ae749e091c6b46c
张应锋 <zhangyingfeng@hogecloud.com>
2019-07-01 10:34:43 +0800

    修改UI和交互更改音视频展示的数据结构

--- src/components/subjects/normal.vue
+++ src/components/subjects/normal.vue
@@ -31,7 +31,7 @@
         :src="annexMedia(media)">
       </my-audio>
       <!--视频播放-->
-      <my-video v-if="mediaKey=='video' && media.length" class="my-video" :poster="annexMedia(media).cover" :src="annexMedia(media).src"></my-video>
+      <my-video v-if="mediaKey=='video' && annexMedia(media)" class="my-video" :poster="annexMedia(media).cover" :src="annexMedia(media).src"></my-video>
     </div>
     <!--每个选择项-->
     <div class="subject-select-wrap" v-for="(optItem,optIndex) in data.options" :key='optIndex' ref="subjectSelectWrap">
@@ -50,7 +50,7 @@
           :src="annexMedia(media)">
         </my-audio>
         <!--视频播放-->
-        <my-video v-if="mediaKey=='video' && media.length" class="my-video" :poster="annexMedia(media).cover" :src="annexMedia(media).src"></my-video>
+        <my-video v-if="mediaKey=='video' && annexMedia(media)" class="my-video" :poster="annexMedia(media).cover" :src="annexMedia(media).src"></my-video>
       </div>
     </div>
     <!--题目解析选项-->
