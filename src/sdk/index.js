aa8bb0878b90a597c915dfd8c6f913d81ba2c11b
xuzhen <xuzhen@hogecloud.com>
2020-06-29 09:19:26 +0800

    提交

---
+++ src/sdk/index.js
@@ -0,0 +1,12 @@
+import * as browser from './browser'
+import * as smartcity from './smartcity'
+import * as wechat from './wechat'
+import * as dingding from './dingding'
+
+export const sdkPlat = {
+  browser,
+  dingding,
+  smartcity,
+  wechat
+}
+
