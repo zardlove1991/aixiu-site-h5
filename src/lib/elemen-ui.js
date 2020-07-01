07133b3b96f31e08332676eebf6c4caf4917b1fa
wangyaru <wangyaru@hoge.cn>
2020-07-01 10:51:40 +0800

    element用到组件引入

--- src/lib/elemen-ui.js
+++ src/lib/elemen-ui.js
@@ -1,10 +1,14 @@
 import 'element-ui/lib/theme-chalk/index.css'
 import {
   Progress,
-  Tooltip
+  Tooltip,
+  Radio,
+  Checkbox
 } from 'element-ui'
 
 export default function (Vue) {
   Vue.use(Progress)
   Vue.use(Tooltip)
+  Vue.use(Radio)
+  Vue.use(Checkbox)
 }
