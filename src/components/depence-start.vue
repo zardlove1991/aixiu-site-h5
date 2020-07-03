8f991fde94b490a41e30ca61d2457e4eba086f0e
xuzhen <xuzhen@hogecloud.com>
2020-07-03 10:44:26 +0800

    微调

--- src/components/depence-start.vue
+++ src/components/depence-start.vue
@@ -73,8 +73,8 @@
       <button class="end-exambtn" v-if ="examInfo.timeStatus == 2">答题已结束</button>
     </div>
     <div class="btn-area" v-else>
-      <button class="start-exambtn" @click.stop="goExamPage" v-if ="examInfo.person_status === 0">{{examInfo.limit.button}}</button>
-      <button class="end-exambtn" v-else>{{examInfo.limit.button}}</button>
+      <button class="start-exambtn" @click.stop="goExamPage" v-if ="examInfo.person_status === 0">{{examInfo.limit.button || '开始答题'}}</button>
+      <button class="end-exambtn" v-else>{{examInfo.limit.button || '开始答题'}}</button>
     </div>
     <my-model
       :show="App"
