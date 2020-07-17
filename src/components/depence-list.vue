e715cd897fafce004e6960cd7788432b5f7cd30c
wangyaru <wangyaru@hoge.cn>
2020-07-17 11:44:55 +0800

    update

--- src/components/depence-list.vue
+++ src/components/depence-list.vue
@@ -273,11 +273,11 @@ export default {
       })
     },
     submitExam () {
-      this.saveAnswerRecords(this.answerList)
+      // this.saveAnswerRecords(this.answerList)
       this.isShowSubmitModel = true
     },
     noEndTime () {
-      this.saveAnswerRecords(this.answerList)
+      // this.saveAnswerRecords(this.answerList)
     },
     endTime () {
       this.isShowSuspendModels = !this.isShowSuspendModels
