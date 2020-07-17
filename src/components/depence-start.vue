fb3f231c2ae2e8d554e831289a88db73c5bebac7
wangyaru <wangyaru@hoge.cn>
2020-07-17 16:25:00 +0800

    圆角

--- src/components/depence-start.vue
+++ src/components/depence-start.vue
@@ -439,6 +439,7 @@ export default {
           height:px2rem(150px);
           background:rgba(255,255,255,0.2);
           color:#fff;
+          border-radius: px2rem(12px);
           @include font-dpr(15px);
           &:nth-child(2){
             margin:0 px2rem(21px);
