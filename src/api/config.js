42f9911b0d828a972aa9db537718bf398ee9a84d
wangyaru <wangyaru@hoge.cn>
2020-07-22 14:21:36 +0800

    配置文件修改

--- src/api/config.js
+++ src/api/config.js
@@ -2,10 +2,11 @@ import { getEnvironment } from '@/utils/app'
 const protocol = window.location.protocol
 const env = getEnvironment()
 const suffix = env === 'online' ? '' : `-${env}`
+const v2 = env === 'online' ? '.v2' : ''
 
 export default {
-  'submit': `${protocol}//result${suffix}.aihoge.com/`,
-  'user': `${protocol}//member${suffix}.aihoge.com/`,
+  'submit': `${protocol}//result${suffix}${v2}.aihoge.com/`,
+  'user': `${protocol}//member${suffix}${v2}.aihoge.com/`,
   'exam': `${protocol}//examinationapi${suffix}.aihoge.com/`,
   'timeout': 36000,
   'HTTP-X-H5-VERSION': 1.0,
