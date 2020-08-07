0b13b8ae9e2503b00629fb3d3fbeba6fb5e26567
wangyaru <wangyaru@hoge.cn>
2020-08-07 09:22:00 +0800

    setClick

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import 'babel-polyfill'
 import router from '@/router/index'
 import store from '@/store/index'
 import { oauth } from '@/utils/userinfo'
-import { setTheme, setBrowserTitle, setPlatCssInclude } from '@/utils/utils'
+import { setTheme, setBrowserTitle, setPlatCssInclude, setClick } from '@/utils/utils'
+import STORAGE from '@/utils/storage'
 // 引入所有第三库
 import '@/lib/index'
 
@@ -13,6 +14,7 @@ Vue.config.productionTip = false
 setPlatCssInclude()
 router.beforeEach((to, from, next) => {
   if ((to.name === 'depencestart' || to.name === 'votebegin') && !from.name) {
+    // 第一次进入页面
     oauth((res) => {
       if (res && res.id) {
         next()
@@ -23,13 +25,21 @@ router.beforeEach((to, from, next) => {
   }
 })
 // 添加路由的钩子函数
-router.afterEach(route => {
+router.afterEach((route, from) => {
   // dynamicTitle 动态标题（某个内容跳转）
   let query = router.history.current.query
   let routerTitle = query && query.title ? query.title : route.meta.title
   // 更改当前网页的title
   setBrowserTitle(routerTitle)
   let name = router.currentRoute.name
+  // 第一次进入投票页
+  if (route.name === 'votebegin' && !from.name) {
+    let detailInfo = STORAGE.get('detailInfo')
+    if (detailInfo && detailInfo.id) {
+      let { id, title, mark } = detailInfo
+      setClick(id, title, mark)
+    }
+  }
   if (name !== 'depencestart' && name !== 'votebegin') {
     let id = router.currentRoute.params.id
     setTheme(id, name)
