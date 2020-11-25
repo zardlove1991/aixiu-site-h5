4a5f7e3cd04ac9d926dcbc76715876cbc67a018b
owen <zhangzhen@hoge.cn>
2020-11-25 16:31:05 +0800

    替换配置接口与接口域名

--- src/api/config.js
+++ src/api/config.js
@@ -1,18 +1,18 @@
 import { getEnvironment } from '@/utils/app'
 const protocol = window.location.protocol
 const env = getEnvironment()
-const suffix = env === 'online' ? '' : `-${env}`
-const v2 = env === 'online' ? '.v2' : ''
+const suffix = env === 'online' ? '-ax-' : `-ax-${env}`
+const host = `${suffix}${v2}.aihoge.com`
 
 export default {
-  'submit': `${protocol}//result${suffix}${v2}.aihoge.com/`,
-  'user': `${protocol}//member${suffix}${v2}.aihoge.com/`,
-  'exam': `${protocol}//examinationapi${suffix}.aihoge.com/`,
-  'vote': `${protocol}//voteapi${suffix}${v2}.aihoge.com/`,
-  'mlink': `${protocol}//m${suffix}${v2}.aihoge.com/`,
-  'reserve': `${protocol}//reserve${suffix}${v2}.aihoge.com/`,
-  'public': `${protocol}//public${suffix}${v2}.aihoge.com/`,
-  'c4': `${protocol}//c4api${suffix}${v2}.aihoge.com/`,
+  'submit': `${protocol}//result${host}/`,
+  'user': `${protocol}//member${host}/`,
+  'exam': `${protocol}//examinationapi${host}/`,
+  'vote': `${protocol}//voteapi${host}/`,
+  'mlink': `${protocol}//m${host}/`,
+  'reserve': `${protocol}//reserve${host}/`,
+  'public': `${protocol}//public${host}/`,
+  'c4': `${protocol}//c4api${host}/`,
   'timeout': 36000,
   'HTTP-X-H5-VERSION': 1.0,
   'X-CLIENT-VERSION': 1314,
