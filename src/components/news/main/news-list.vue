52249f55990b14c17032d72b16f301c4aa0eceac
wangyaru <wangyaru@hoge.cn>
2020-11-25 18:04:37 +0800

    update

--- src/components/news/main/news-list.vue
+++ src/components/news/main/news-list.vue
@@ -108,7 +108,7 @@ export default {
       let calcHeight = baseHeight / 2 // 滚动差x像素时进入下一页
       console.log('handleScroll', ',baseHeight= ' + baseHeight, ',calcHeight= ' + calcHeight, ',top= ' + top)
       let scroll = top - this.tmpHight
-      let nodes = document.getElementsByClassName('news-start-wrap')
+      // let nodes = document.getElementsByClassName('news-start-wrap')
       this.tmpHight = top
       if (scroll < 0) {
         // 向上滚动
