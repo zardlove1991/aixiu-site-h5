369adf8cb656cf3206196d99ed688488c4daf747
mac <wanglemei@hoge.cn>
2020-12-29 17:13:19 +0800

    【修改】打包配置

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -64,6 +64,11 @@ const devWebpackConfig = merge(baseWebpackConfig, {
         from: path.resolve(__dirname, '../static'),
         to: config.dev.assetsSubDirectory,
         ignore: ['.*']
+      },
+      {
+        from: path.resolve(__dirname, '../waitting.html'),
+        to: config.build.assetsRoot,
+        ignore: ['.*']
       }
     ])
   ]
