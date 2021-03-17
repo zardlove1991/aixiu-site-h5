0e64860ccb64d1f681c8e49a6129bdaa22db6c9f
wangyaru <wangyaru@hoge.cn>
2021-03-17 15:09:51 +0800

    count

--- src/components/live-exam/main/live-start.vue
+++ src/components/live-exam/main/live-start.vue
@@ -56,7 +56,7 @@
           <button class="start-exambtn" @click.stop="isShowPassword()" v-if="examInfo.remain_counts !== 0 || isNoLimit">{{examInfo.limit.button || '开始答题'}}</button>
           <button class="end-exambtn" v-else>{{examInfo.limit.button || '开始答题'}}</button>
         </div>
-        <div class="start-exam-tips" v-if="!isNoLimit">答题规范：每个用户最多提交{{examSubmitCount}}次</div>
+        <div class="start-exam-tips" v-if="!isNoLimit">答题规范：每天最多提交{{examSubmitCount}}次<span v-if="examSubmitCount2">，活动期间最多提交{{examSubmitCount2}}次</span></div>
       </div>
     </div>
     <my-model
@@ -179,23 +179,26 @@ export default {
   components: { MyModel, DrawCheckDialog, LiveVideo, LinkDialog, PopDialog, LuckDrawDialog },
   computed: {
     ...mapGetters('depence', ['examInfo', 'answerCardInfo', 'luckDrawLink']),
+    examSubmitCount2 () {
+      let examInfo = this.examInfo
+      let count = 1
+      if (examInfo && examInfo.limit) {
+        let userIdLimit = examInfo.limit.userid_limit_num
+        if (userIdLimit) {
+          userIdLimit = parseInt(userIdLimit)
+          count = userIdLimit
+        }
+      }
+      return count
+    },
     examSubmitCount () {
       let examInfo = this.examInfo
       let count = 1
       if (examInfo && examInfo.limit) {
-        let {
-          day_userid_limit_num: dayUserIdLimit,
-          ip_limit_num: ipLimit,
-          userid_limit_num: userIdLimit
-        } = examInfo.limit
-        if (dayUserIdLimit && dayUserIdLimit > count) {
+        let dayUserIdLimit = examInfo.limit.day_userid_limit_num
+        if (dayUserIdLimit) {
+          dayUserIdLimit = parseInt(dayUserIdLimit)
           count = dayUserIdLimit
-        }
-        if (ipLimit && ipLimit > count) {
-          count = ipLimit
-        }
-        if (userIdLimit && userIdLimit > count) {
-          count = userIdLimit
         }
       }
       return count
