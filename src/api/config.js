0c498a343f81740320c946597526a068935fd41a
陈梦洁 <chenmengjie@hoge.cn>
2021-04-08 16:52:19 +0800

    修改厦门接口域名

--- src/api/config.js
+++ src/api/config.js
@@ -7,21 +7,21 @@ const suffix = env === 'online' ? '' : env === 'pre' ? '-ax-pre' : `-${env}`
 const v2 = env === 'online' ? '-ax' : ''
 
 export default {
-  'submit': `${protocol}//result${suffix}${v2}.aihoge.com/`,
-  'user': `${protocol}//member${suffix}${v2}.aihoge.com/`,
-  'exam': `${protocol}//examinationapi${suffix}.aihoge.com/`,
-  'vote': `${protocol}//voteapi${suffix}${v2}.aihoge.com/`,
-  'mlink': `${protocol}//m${suffix}${v2}.aihoge.com/`,
-  'reserve': `${protocol}//reserve${suffix}${v2}.aihoge.com/`,
-  'public': `${protocol}//public${suffix}${v2}.aihoge.com/`,
-  'c4': `${protocol}//c4api${suffix}${v2}.aihoge.com/`,
-  'news': `${protocol}//news${suffix}${v2}.aihoge.com/`,
+  'submit': `${protocol}//result${suffix}${v2}.kxm.xmtv.cn/`,
+  'user': `${protocol}//member${suffix}${v2}.kxm.xmtv.cn/`,
+  'exam': `${protocol}//examinationapi${suffix}.kxm.xmtv.cn/`,
+  'vote': `${protocol}//voteapi${suffix}${v2}.kxm.xmtv.cn/`,
+  'mlink': `${protocol}//m${suffix}${v2}.kxm.xmtv.cn/`,
+  'reserve': `${protocol}//reserve${suffix}${v2}.kxm.xmtv.cn/`,
+  'public': `${protocol}//public${suffix}${v2}.kxm.xmtv.cn/`,
+  'c4': `${protocol}//c4api${suffix}${v2}.kxm.xmtv.cn/`,
+  'news': `${protocol}//news${suffix}${v2}.kxm.xmtv.cn/`,
   'timeout': 10000,
   'HTTP-X-H5-VERSION': 1.0,
   'X-CLIENT-VERSION': 1314,
   'X-DEVICE-ID': '000',
   'APPID': env === 'test' ? 'wx025937621152c396' : 'wx63a3a30d3880a56e',
   'COMP_APPID': 'wx566758628b7a3fa2',
-  'REDIRECT-URI': env === 'test' ? `${protocol}//h5-ceshi.aihoge.com/bridge/index.html` : `${protocol}//xzh5.hoge.cn/bridge/index.html`,
+  'REDIRECT-URI': env === 'test' ? `${protocol}//h5-ceshi.kxm.xmtv.cn/bridge/index.html` : `${protocol}//xzh5.hoge.cn/bridge/index.html`,
   'OPEN_NEW_PAGE': ['api/client/examination/{id}', 'open/examination/detail', 'h5/signature', 'signature', 'client/voting/base/{id}', 'client/voting/{id}', 'client/order/{id}', 'client/news/{id}/']
 }
