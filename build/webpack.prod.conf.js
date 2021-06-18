27eaa337b11cd9f8494838970e84fcf06927eead
陈梦洁 <chenmengjie@hoge.cn>
2021-06-18 18:01:36 +0800

    调整

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -73,7 +73,8 @@ const webpackConfig = merge(baseWebpackConfig, {
       },
       // necessary to consistently work with multiple chunks via CommonsChunkPlugin
       chunksSortMode: 'dependency',
-      projectPath: process.env.BUILD_TARGET === 'pre' ? '/pre' : ''
+      // projectPath: process.env.BUILD_TARGET === 'pre' ? '/pre' : ''
+      projectPath: '/'
     }),
     // keep module.id stable when vendor modules does not change
     new webpack.HashedModuleIdsPlugin(),
