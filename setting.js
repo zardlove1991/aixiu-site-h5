040823d9217eee5fe258b8337a6b33f2f9f1392d
owen <zhangzhen@hoge.cn>
2021-07-27 18:03:02 +0800

    恢复 setting.js走 .env.example.dev  和 online动态替换

--- setting.js
+++ setting.js
@@ -1,21 +1,11 @@
-// var config = {
-//   CUSTOM_HOST: '{{CUSTOM_HOST}}',
-//   CUSTOM_APPID: '{{CUSTOM_APPID}}',
-//   CUSTOM_COMP_APPID: '{{CUSTOM_COMP_APPID}}',
-//   CUSTOM_REDIRECT_URI: '{{CUSTOM_REDIRECT_URI}}',
-//   CUSTOM_PLAT: '{{CUSTOM_PLAT}}'
-// }
 var config = {
-  CUSTOM_HOST: '-ax.aihoge.com',
-  CUSTOM_APPID: 'wx2e98fb1d45a98f73',
-  CUSTOM_COMP_APPID: 'wx566758628b7a3fa2',
-  CUSTOM_REDIRECT_URI: '//axh5.aihoge.com/bridge.html',
-  CUSTOM_PLAT: 'online',
-  PROJECT_PlATFORM: 'fujian-answer'
+  CUSTOM_HOST: '{{CUSTOM_HOST}}',
+  CUSTOM_APPID: '{{CUSTOM_APPID}}',
+  CUSTOM_COMP_APPID: '{{CUSTOM_COMP_APPID}}',
+  CUSTOM_REDIRECT_URI: '{{CUSTOM_REDIRECT_URI}}',
+  CUSTOM_PLAT: '{{CUSTOM_PLAT}}',
+  PROJECT_PlATFORM: '{{PROJECT_PlATFORM}}'
 }
-
 window.$axGlobalConfig = config
-
-console.log('settingConfig-------', config)
 
 localStorage.setItem(location.host, JSON.stringify(config))
