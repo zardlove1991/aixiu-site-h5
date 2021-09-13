0690b2ff4221bdc708b53aed7efb233a7f6b15be
fengjutian <fjutian@foxmail.com>
2021-09-13 10:25:23 +0800

    save

---
+++ src/components/vote/global/vote-reward.vue
@@ -0,0 +1,24 @@
+<template>
+<div class='vote-reward-wrap'>
+
+
+</div>
+</template>
+
+<script>
+import TipsDialog from '@/components/vote/global/tips-dialog'
+export default {
+  data () {
+    return {
+
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
