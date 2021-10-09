1ccdda4c54b3f8d7bf631437dfbe4004b557093a
zard1991 <843249072@qq.com>
2021-10-09 21:50:12 +0800

    调整提示

--- src/components/vote/main/vote-submit.vue
+++ src/components/vote/main/vote-submit.vue
@@ -41,8 +41,8 @@
         <div class="form-title">上传图片</div>
         <div class="form-tips-div" v-if="imageRatio && !imageUpload_min_num">建议比例：4:5.6（1寸照片的比例尺寸），小于10M;图片最多上传{{imageUpload_max_num}}张；支持PNG、JPG、GIF格式</div>
         <div class="form-tips-div" v-else-if="imageRatio && imageUpload_min_num">建议比例：4:5.6（1寸照片的比例尺寸,小于10M；图片最少上传{{imageUpload_min_num}}张,最多上传{{imageUpload_max_num}}张；支持PNG、JPG、GIF格式</div>
-         <div class="form-tips-div" v-else-if="!imageRatio && imageUpload_min_num">建议比例：1:1，小于10M;图片最少上传{{imageUpload_min_num}}张,最多上传{{imageUpload_max_num}}张；支持PNG、JPG、GIF格式</div>
-        <div class="form-tips-div" v-else>建议比例：1:1，小于10M；图片最多上传9张；支持PNG、JPG、GIF格式</div>
+         <div class="form-tips-div" v-else-if="!imageRatio && imageUpload_min_num">小于10M;图片最少上传{{imageUpload_min_num}}张,最多上传{{imageUpload_max_num}}张；支持PNG、JPG、GIF格式</div>
+        <div class="form-tips-div" v-else>小于10M；图片最多上传9张；支持PNG、JPG、GIF格式</div>
         <div class="form-content">
           <file-upload
             ref="picture-file-upload"
