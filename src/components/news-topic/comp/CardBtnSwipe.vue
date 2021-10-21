ad86be5fde840ab0aa95423f4c80a7e8dd92c9ad
fengjutian <fjutian@foxmail.com>
2021-10-21 16:31:06 +0800

    save

--- src/components/news-topic/comp/CardBtnSwipe.vue
+++ src/components/news-topic/comp/CardBtnSwipe.vue
@@ -9,22 +9,66 @@
         @click='choiceColumnList(item)'
         :class='{
           "choiced-btn" : item.isChecked,
-          "circle-box": columnStyleValue == 1}'
+          "circle-box": columnStyleValue == 1,
+          "low-circle-box": columnStyleValue == 2
+        }'
         class='single-btn-wrap'>{{item.title}}</div>
     </template>
 
     <!-- 多行 -->
     <template v-if='columnChangeStatus === 2'>
-      <div class='mul-card-wrap'>
+      <div v-for='(item, index) in columnList' :key='index' :class='{"box-groups": columnList.length > 1}' class='mul-card-wrap'>
         <div class='mul-card-line'>
-          <div class='single-card'>媒体关注</div>
-          <div class='single-card'>媒体媒体关注媒体关注媒体关注关注</div>
-          <div class='single-card'>媒体关注</div>
+          <div v-if='item[0] !== undefined'
+            @click='choiceColumnList(item[0])'
+            :class='{
+              "choiced-btn" : item[0].isChecked,
+              "circle-box": columnStyleValue == 1,
+              "low-circle-box": columnStyleValue == 2
+            }'
+            class='single-card'>{{item[0].title}}</div>
+          <div v-if='item[1] !== undefined'
+            @click='choiceColumnList(item[1])'
+            :class='{
+              "choiced-btn" : item[1].isChecked,
+              "circle-box": columnStyleValue == 1,
+              "low-circle-box": columnStyleValue == 2
+            }'
+            class='single-card'>{{item[1].title}}</div>
+          <div v-if='item[2] !== undefined'
+            @click='choiceColumnList(item[2])'
+            :class='{
+              "choiced-btn" : item[2].isChecked,
+              "circle-box": columnStyleValue == 1,
+              "low-circle-box": columnStyleValue == 2
+            }'
+            class='single-card'>{{item[2].title}}</div>
         </div>
         <div class='mul-card-line'>
-          <div class='single-card' :class='{"choiced-btn" : true}'>媒体关注</div>
-          <div class='single-card'>媒体媒体关注媒体关注媒体关注关注</div>
-          <div class='single-card'>媒体关注</div>
+          <div v-if='item[3] !== undefined'
+            @click='choiceColumnList(item[3])'
+            :class='{
+              "choiced-btn" : item[3].isChecked,
+              "circle-box": columnStyleValue == 1,
+              "low-circle-box": columnStyleValue == 2
+            }'
+            class='single-card'>{{item[3].title}}</div>
+          <div v-if='item[4] !== undefined'
+            @click='choiceColumnList(item[4])'
+            :class='{
+              "choiced-btn" : item[4].isChecked,
+              "circle-box": columnStyleValue == 1,
+              "low-circle-box": columnStyleValue == 2
+            }'
+            class='single-card'>{{item[4].title}}</div>
+          <div v-if='item[5] !== undefined'
+            @click='choiceColumnList(item[5])'
+            :class='{
+              "choiced-btn" : item[5].isChecked,
+              "circle-box": columnStyleValue == 1,
+              "low-circle-box": columnStyleValue == 2
+            }'
+            class='single-card'>{{item[5].title}}</div>
         </div>
       </div>
     </template>
@@ -48,31 +92,35 @@ export default {
     activeObj: {
       handler (newData, oldData) {
         this.columnChangeStatus = newData.topic_display.column_change_status
+        let columnListObj = []
+        columnListObj = JSON.parse(JSON.stringify(newData.column_set.column_list))
         this.columnList = []
-        this.columnList = JSON.parse(JSON.stringify(newData.column_set.column_list))
         if (this.columnChangeStatus === 1) {
           // 单行
+          this.columnList = columnListObj
           for (let i of this.columnList) {
             this.$set(i, 'isChecked', false)
           }
+          this.columnList[0].isChecked = true
         } else if (this.columnChangeStatus === 2) {
           // 多行
           // 每6个位一组
-          // let _middleArr = []
-          // for (let i of this.columnList) {
-          //   this.$set(i, 'isChecked', false)
-          //   _middleArr.push(i)
-          //   if ((i + 1) % 6 === 0) {
-          //     this.columnList.push(_middleArr)
-          //     _middleArr = []
-          //   }
-          // }
-          // console.log('777', this.columnList)
+          if (columnListObj.length < 6) {
+            for (let i of columnListObj) {
+              this.$set(i, 'isChecked', false)
+            }
+            this.columnList.push(columnListObj)
+          } else if (columnListObj.length >= 6) {
+            let loopNum = Math.ceil(columnListObj.length / 6)
+            columnListObj.forEach(item => {
+              this.$set(item, 'isChecked', false)
+            })
+            for (let i = 0; i < loopNum; i++) {
+              this.columnList.push(columnListObj.splice(0, 6))
+            }
+          }
+          this.columnList[0][0].isChecked = true
         }
-
-        this.columnList[0].isChecked = true
-
-        console.log('999', this.columnList)
       },
       deep: true
     },
@@ -81,7 +129,7 @@ export default {
         let _topicDisplay = newData.limit.topic_display
         this.topicDisplayValue = _topicDisplay.topic_display_value
         this.columnStyleValue = _topicDisplay.column_style_value
-        console.log('newData----', newData.limit.topic_display.column_style_value)
+        console.log('this.columnStyleValue', this.columnStyleValue)
       },
       deep: true
     }
@@ -96,7 +144,6 @@ export default {
     }
   },
   mounted () {
-    console.log('columnList', this.columnList)
     this.topicDisplayValue = ''
   },
   methods: {
@@ -127,11 +174,12 @@ export default {
     overflow-y: hidden;
     display:-webkit-box;
     .circle-box {
-      border-radius: px2rem(32px);
+      border-radius: px2rem(32px) !important;
     }
-    .square-box {
-      border-radius: px2rem(8px);
+    .low-circle-box {
+      border-radius: px2rem(8px) !important;
     }
+
     .single-btn-wrap{
       float: none;
       width: px2rem(172px);
@@ -154,6 +202,10 @@ export default {
     }
   }
 
+  .box-groups{
+    margin-right: px2rem(20px);
+  }
+
   .mul-card-wrap{
     .mul-card-line + .mul-card-line{
       margin-top: px2rem(20px);
@@ -162,7 +214,6 @@ export default {
       display: flex;
       flex-direction: row;
       .single-card{
-        flex: 1;
         width: px2rem(210px);
         height: px2rem(64px);
         line-height: px2rem(64px);
